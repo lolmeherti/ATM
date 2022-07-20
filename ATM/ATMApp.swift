bc6c0b6ba2692d7233b8ce6da2d513903e8e240c
lolmeherti <lolmeherti@vusra.com>
2022-07-20 07:43:33 +0200

    initial commit

---
+++ ATM/ATMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATMApp.swift
+//  ATM
+//
+//  Created by Swift Developer on 20.07.22.
+//
+
+import SwiftUI
+
+@main
+struct ATMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ATM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ATM
+//
+//  Created by Swift Developer on 20.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
