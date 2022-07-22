ed8b60058b8d35f93279e90ac8ace88e3b7f2af4
SwiftDev <nyeknyekereg@gmail.com>
2022-07-22 09:12:04 +0200

    [ATM] ADDS shell for CreditCardValidator

---
+++ ATM/Validators/CreditCardValidator.swift
@@ -0,0 +1,20 @@
+//
+//  CreditCardValidator.swift
+//  ATM
+//
+//  Created by Swift Developer on 22.07.22.
+//
+
+import Foundation
+import SwiftUI
+
+struct CreditCardValidator {
+    
+    let accountNumber: String
+    
+    public func validateAccountNumber(_ accountNumber: String){
+        for (index, number) in accountNumber.enumerated() {
+            
+        }
+    }
+}
