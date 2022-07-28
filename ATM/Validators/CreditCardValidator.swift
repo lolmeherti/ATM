b85542dc7fee7d75d3da71d609aeda6cb27e11f2
SwiftDev <nyeknyekereg@gmail.com>
2022-07-28 08:30:10 +0200

    [ATM] ADDS a validator for credit card numbers

--- ATM/Validators/CreditCardValidator.swift
+++ ATM/Validators/CreditCardValidator.swift
@@ -8,13 +8,29 @@
 import Foundation
 import SwiftUI
 
+let amountOfDigitsInAccountNumberCountingHyphens = 19
+let amountOfDigitsInAccountNumber = 16
+
 struct CreditCardValidator {
-    
-    let accountNumber: String
-    
-    public func validateAccountNumber(_ accountNumber: String){
-        for (index, number) in accountNumber.enumerated() {
-            
+
+    //this function will check whether the account number is a valid one
+    //returns true if the account is valid, returns false if it isnt
+    //makes sure the user-input is numbers only
+    //makes sure the user-input is the correct length
+    public func validateAccountNumber(_ accountNumber: String) -> Bool {
+        
+        let sanitizedAccountNumber = accountNumber.digits
+        
+        if(sanitizedAccountNumber.count <= amountOfDigitsInAccountNumberCountingHyphens && sanitizedAccountNumber.count >= amountOfDigitsInAccountNumber) {
+            for (_, number) in sanitizedAccountNumber.enumerated() {
+                
+                if(number.isNumber) {
+                    
+                    return true
+                }
+            }
         }
+        return false
     }
 }
+
