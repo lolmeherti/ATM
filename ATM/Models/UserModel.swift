b0c4e0e5e03b983c337ef48216a0220ede743e2b
SwiftDev <nyeknyekereg@gmail.com>
2022-08-09 09:52:21 +0200

    [ATM] ADDS UserModel

--- ATM/Models/UserModel.swift
+++ ATM/Models/UserModel.swift
@@ -8,3 +8,18 @@
 import Foundation
 import Firebase
 	
+struct UserModel: Identifiable {
+    var id:String
+    var first_name:String
+    var last_name:String
+    var passport_id:String
+    var email:String
+    var phone_number:String
+    var date_of_birth:Date
+    var address:String
+    var country:String
+    var timestamp:Date
+    var role:String
+
+    
+}
