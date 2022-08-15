fb03d5060d9e7ba3d98a801120a4dd8d2aea2feb
SwiftDev <nyeknyekereg@gmail.com>
2022-08-15 10:03:51 +0200

    [ATM] ADDS gradient background color to new view

--- ATM/Views/AccountView.swift
+++ ATM/Views/AccountView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct AccountView: View {
+    
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color("Charcoal"), Color("DarkColorGradient")]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.vertical)
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            
+        }
     }
-}
-
-struct AccountView_Previews: PreviewProvider {
-    static var previews: some View {
-        AccountView()
+    
+    struct AccountView_Previews: PreviewProvider {
+        static var previews: some View {
+            AccountView()
+        }
     }
 }
