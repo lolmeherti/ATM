ab366549dab86dabff18727e961ba82dcc2666b7
SwiftDev <nyeknyekereg@gmail.com>
2022-08-15 10:07:33 +0200

    [ATM] CHANGES loginUser() links to database and verifies credentials

--- ATM/ViewModels/UserViewModel.swift
+++ ATM/ViewModels/UserViewModel.swift
@@ -103,18 +103,36 @@ class UserViewModel: ObservableObject{
         }
     }
     
-    
-    public func loginUser(accountNumber:String, pinCode:String) -> Bool{
+    //---LOGS USER IN IF THE ACCOUNT NUMBER AND THE PIN CODE MATCH A DATABASE ENTRY---//
+    public func loginUser(userInputAccountNumber:String, userInputPinCode:String, completion: @escaping (Bool) -> Void) {
+        let db = Firestore.firestore()
+        let dbQuery = db.collection("card_table").whereField("card_account_number", isEqualTo: userInputAccountNumber)
         
         let creditCardValidator = CreditCardValidator()
         
-        let accountNumberValid:Bool = creditCardValidator.validateAccountNumber(accountNumber)
+        let accountNumberValid:Bool = creditCardValidator.validateAccountNumber(userInputAccountNumber)
         
         if(accountNumberValid){
-            //TODO
-            //missing comparison code to database
-            return true
+            dbQuery.getDocuments() {(documents, error) in
+                if(error != nil) {
+                    print("Error: \(error?.localizedDescription ?? "")")
+                }
+                
+                var pinCode:String = ""
+                
+                for document in documents!.documents{
+                    pinCode = document.get("card_pin_code") as? String ?? ""
+                }
+               
+                if(userInputPinCode == pinCode){
+                    completion(true)
+                    return
+                } else {
+                    completion(false)
+                }
+            }
         }
-        return false
     }
+    
 }
+
