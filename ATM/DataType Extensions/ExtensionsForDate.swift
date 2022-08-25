87e600f7635b2a82b33314877dd6bd824b29c184
SwiftDev <nyeknyekereg@gmail.com>
2022-08-25 07:40:25 +0200

    [ATM] ADDS extension for custom date object creation

---
+++ ATM/DataType Extensions/ExtensionsForDate.swift
@@ -0,0 +1,19 @@
+//
+//  ExtensionsForDate.swift
+//  ATM
+//
+//  Created by Swift Developer on 24.08.22.
+//
+
+import Foundation
+
+extension Date  {
+    static func from(year: Int, month: Int, day: Int) -> Date? {
+            let calendar = Calendar(identifier: .gregorian)
+            var dateComponents = DateComponents()
+            dateComponents.year = year
+            dateComponents.month = month
+            dateComponents.day = day
+        return calendar.date(from: dateComponents) ?? nil
+    }
+}
