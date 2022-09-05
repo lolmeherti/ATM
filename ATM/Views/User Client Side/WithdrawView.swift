da084ec43f43cc3853a63fb6c3ba42e8e10af117
SwiftDev <deampuleadd@yandex.com>
2022-09-05 08:44:02 +0200

    [ATM] ADDS view for withdrawing funds from bank account

---
+++ ATM/Views/User Client Side/WithdrawView.swift
@@ -0,0 +1,83 @@
+//
+//  WithdrawView.swift
+//  ATM
+//
+//  Created by Swift Developer on 25.08.22.
+//
+
+import SwiftUI
+
+struct WithdrawView: View {
+    @EnvironmentObject var userInstance:UserViewModel
+    @State var withdrawAmount:Double = 0
+    @State var showBalance:Double = 0
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color("Charcoal"), Color("DarkColorGradient")]), startPoint: .top, endPoint: .leading)
+                .edgesIgnoringSafeArea(.vertical)
+            
+            ScrollView{
+                VStack(alignment: .center, spacing: 25){
+                        Text("Your current balance:")
+                            .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                            .offset(x: 0, y: 80)
+                        
+                    Text("\(showBalance > 0 ? showBalance : userInstance.currentUser.balance, specifier: "%.2f")")
+                            .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color("LighterYellow"))
+                            .multilineTextAlignment(.center)
+                            .offset(x: 0, y: 60)
+                            
+                        Spacer()
+                        Text("Amount to withdraw")
+                            .font(/*@START_MENU_TOKEN@*/.title2/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                            .multilineTextAlignment(.center)
+                            .offset(x: 0, y: 105)
+                        
+                        
+                        TextField("Amount to withdraw", value: $withdrawAmount, format:.number)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal, 8)
+                            .padding(.bottom, 20)
+                            .offset(x: 0, y: 95)
+                            .keyboardType(.decimalPad)
+
+                        Spacer()
+                        
+                        Button{
+                            CreditCardViewModel().withdrawFromAccountBalance(accountNumber: userInstance.currentUser.accountNumber, withdrawAmount: withdrawAmount){ currentBalance in
+                                showBalance = currentBalance
+                            }
+                        } label: {
+                            Text("Withdraw")
+                                .fontWeight(.semibold)
+                                .font(.title)
+                        }
+                        .padding([.horizontal], 50)
+                        .padding([.vertical], 10)
+                        
+                        .foregroundColor(Color("DarkColorGradient"))
+                        .background(LinearGradient(gradient: Gradient(colors: [Color("Yellow"), Color("LighterYellow")]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(25)
+                        .offset(x: 0, y: 250)
+                    }
+            }
+        }
+        .onTapGesture {
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to:nil, from:nil, for:nil)
+                }
+    }
+}
+
+struct WithdrawView_Previews: PreviewProvider {
+    static var previews: some View {
+        WithdrawView()
+    }
+}
