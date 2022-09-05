2c771a44d2faa608305373827029ed6caa514edb
SwiftDev <deampuleadd@yandex.com>
2022-09-05 08:45:12 +0200

    [ATM] FIXES static userdata, makes sure instance updates values

--- ATM/Views/User Client Side/AccountInfoView.swift
+++ ATM/Views/User Client Side/AccountInfoView.swift
@@ -8,9 +8,7 @@
 import SwiftUI
 
 struct AccountInfoView: View {
-    
     @EnvironmentObject var userInstance:UserViewModel
-    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [Color("Charcoal"), Color("DarkColorGradient")]), startPoint: .top, endPoint: .leading)
@@ -32,7 +30,7 @@ struct AccountInfoView: View {
                         Text("IBAN:")
                             .shadow(color: Color.black.opacity(0.5), radius: 1, x: 0, y: 0)
                             .foregroundColor(.white)
-                        Text(userInstance.currentUser.accountNumber)
+                        Text(self.userInstance.currentUser.accountNumber)
                             .foregroundColor(Color("DarkColorGradient"))
                             .font(.system(size:20, weight: .bold, design: .rounded))
                         
@@ -43,13 +41,13 @@ struct AccountInfoView: View {
                         Text("Valid:")
                             .foregroundColor(.white)
                             .shadow(color: Color.black.opacity(0.5), radius: 1, x: 0, y: 0)
-                        Text(userInstance.currentUser.card_expiration_date, style: .date)
+                        Text(self.userInstance.currentUser.card_expiration_date, style: .date)
                             .foregroundColor(Color("DarkColorGradient"))
                         Spacer()
                         Text("CVC:")
                             .foregroundColor(.white)
                             .shadow(color: Color.black.opacity(0.5), radius: 1, x: 0, y: 0)
-                        Text(userInstance.currentUser.cvc)
+                        Text(self.userInstance.currentUser.cvc)
                             .foregroundColor(Color("DarkColorGradient"))
                     }
                     .offset(x: 0, y: -25)
@@ -57,12 +55,11 @@ struct AccountInfoView: View {
                     
                     
                     HStack{
-                        Text(userInstance.currentUser.firstName)
-                        Text(userInstance.currentUser.lastName)
+                        Text(self.userInstance.currentUser.firstName)
+                        Text(self.userInstance.currentUser.lastName)
                     }
                     .font(.system(size:22, weight: .bold, design: .rounded))
-                    .shadow(color: Color.black.opacity(0.5), radius: 1, x: 0, y: 0)
-                    .foregroundColor(.white)
+                    .foregroundColor(Color("DarkColorGradient"))
                     .offset(y: -10)
                     
                     
@@ -70,13 +67,11 @@ struct AccountInfoView: View {
                         Text("Balance:")
                             .foregroundColor(.white)
                             .shadow(color: Color.black.opacity(0.5), radius: 1, x: 0, y: 0)
-                        Text(String(userInstance.currentUser.balance))
+                        Text("\(self.userInstance.currentUser.balance, specifier: "%.2f")")
                             .foregroundColor(Color("DarkColorGradient"))
                             .font(.system(size:20, weight: .bold, design: .rounded))
                     }
                     .offset(x: 70, y: 20)
-                    
-                    
                 }
                 .font(.system(size:16, weight: .bold, design: .rounded))
                 .foregroundColor(.white)
@@ -84,6 +79,13 @@ struct AccountInfoView: View {
             .offset(x: 0, y: -220)
             .padding()
             
+        }
+        .onAppear{
+            userInstance.getUserDetailsByAccountNumber(accountNumber: userInstance.currentUser.accountNumber) {
+                userDetails in
+                userInstance.setCurrentUserDetails(userDetails: userDetails)
+                userInstance.forceReload.toggle()
+            }
         }
     }
 }
