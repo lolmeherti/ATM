bd961f6bc5be308c53eb0f7a8861cefbd120af8e
SwiftDev <deampuleadd@yandex.com>
2022-09-12 09:55:04 +0200

    [ATM] ADDS function to log transfers

--- ATM/ViewModels/TransactionsViewModel.swift
+++ ATM/ViewModels/TransactionsViewModel.swift
@@ -35,4 +35,25 @@ class TransactionsViewModel: Identifiable, ObservableObject {
             "transaction_date":Date()
         ])
     }
+    
+    func logTransfer(userId:String,
+                     recipientFirstName:String,
+                     recipientLastName:String,
+                     transferSubject:String,
+                     transferAmount:Double)
+    {
+        
+        let db = Firestore.firestore()
+        
+        db.collection("transaction_table").addDocument(data:
+        [
+            "user_foreign_id":userId,
+            "transaction_recipient_last_name":recipientLastName,
+            "transaction_recipient_first_name":recipientFirstName,
+            "transaction_subject":transferSubject,
+            "transaction_amount":transferAmount,
+            "transaction_type":"Transfer",
+            "transaction_date":Date()
+        ])
+    }
 }
