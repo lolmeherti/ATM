368284838b58d96e3ade09d20b0e8f7523572f5a
SwiftDev <deampuleadd@yandex.com>
2022-09-13 10:18:22 +0200

    [ATM] ADDS method to transfer balance from one acc to another

--- ATM/ViewModels/TransactionsViewModel.swift
+++ ATM/ViewModels/TransactionsViewModel.swift
@@ -56,4 +56,30 @@ class TransactionsViewModel: Identifiable, ObservableObject {
             "transaction_date":Date()
         ])
     }
+    
+    //executes callbacks needed for the transfer to complete
+    
+    func transferBalance(senderAccountNumber:String, recipientAccountNumber:String, transferAmount:Double, completion: @escaping (Bool) -> Void){
+        
+        //check if the recipients account number is registered in the database
+        let db = Firestore.firestore()
+        db.collection("card_table").whereField("card_account_number", isEqualTo: recipientAccountNumber).getDocuments { documents, error in
+            for document in documents!.documents {//if the recipient is found in the  database, we loop once. if the recipient is not found, we never loop.
+                
+                //its okay to have this execute inside the loop, because our account number is always unique and there will only ever be one record
+                //we are simply withdrawing the amount out of the current users balance here
+                CreditCardViewModel().withdrawFromAccountBalance(accountNumber: senderAccountNumber, withdrawAmount: transferAmount) { unusedNewBalanceFromCompletion in }//as the naming says, we dont need to escape new balance in this case
+                
+                //then we are depositing the same amount to the recipients account
+                CreditCardViewModel().depositToAccountBalance(accountNumber: recipientAccountNumber, depositAmount: transferAmount) { unusedNewBalanceFromCompletion in } //same as above
+                
+                //we escape true if the transfer succeeded
+                completion(true)
+            }
+        }
+        
+        //if the transfer fails, it means we havent found the recipient inside our database
+        //we escape false in this case
+        completion(false)
+    }
 }
