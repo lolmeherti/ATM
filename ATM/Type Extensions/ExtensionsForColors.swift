8dee69dc1d924107080f47e9db17698778b36606
SwiftDev <deampuleadd@yandex.com>
2022-09-16 09:05:46 +0200

    [ATM] ADDS extension file for colors

---
+++ ATM/Type Extensions/ExtensionsForColors.swift
@@ -0,0 +1,14 @@
+//
+//  ExtensionsForColors.swift
+//  ATM
+//
+//  Created by Swift Developer on 15.09.22.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color{
+    static let darkColorGradient = Color("DarkColorGradient")
+}
